5bddeca19258256d447983176ccf4effd087f81a
vilyever <250450035@163.com>
2016-08-05 15:49:08 +0800

    first release

---
+++ README.md
@@ -0,0 +1,17 @@
+# objcLog
+objcLog
+
+## Import
+### [CocoaPods](http://cocoapods.org)
+use_frameworks!
+</br>
+pod 'objcLog'
+
+### [Carthage](https://github.com/Carthage/Carthage)
+github "vilyever/objcLog" "master"
+
+## Usage
+
+## License
+
+[MIT license](LICENSE)
--- objcLog/objcLog.h
+++ objcLog/objcLog.h
@@ -16,4 +16,8 @@ FOUNDATION_EXPORT const unsigned char objcLogVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <objcLog/PublicHeader.h>
 
-
+#pragma mark VDLog
+#if !VDLog
+#define VDLog(frmt, ...) \
+NSLog(@"%@    【Method:(%s) —— Line:%d】", [NSString stringWithFormat:frmt, ##__VA_ARGS__], __FUNCTION__, __LINE__)
+#endif
